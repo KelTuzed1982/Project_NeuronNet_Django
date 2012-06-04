1413d2587333e901ad56cc9fb02b7261eab21eb5
Chesnokov E. N. <dekkka@mail.ru>
2012-06-05 00:19:42 +0600

    Добавлены комментарии, подправлен код в некоторых местах

--- main/test.py
+++ main/test.py
@@ -1,28 +1,25 @@
+# -*- coding: utf-8 -*-
 import sys
-sys.path.append( 'D:\djNeuronNet\main' )
 
 from NeuronNet_djang import NeuronNet
 
-'''
+'''Тестируем обучение
+print '==========ДО ОБУЧЕНИЯ:==========='
+Net = NeuronNet( cntLayer = 3, nrsInLayer = [ 10, 5, 1 ], inputs = 1 )
 print 'Net.calc( [90] ) = ' + str( Net.calc( [90] ) ) 
 print 'Net.calc( [30] ) = ' + str( Net.calc( [30] ) )
 print 'Net.calc( [0] ) = ' + str( Net.calc( [0] ) )
 Net.learn( 'learn.txt' )
-print '==========IS LEARNED==========='
+print '==========ПОСЛЕ ОБУЧЕНИЯ:==========='
 print 'Net.calc( [90] ) = ' + str( Net.calc( [90] ) ) 
 print 'Net.calc( [30] ) = ' + str( Net.calc( [30] ) )
 print 'Net.calc( [0] ) = ' + str( Net.calc( [0] ) )
-#'''
-'''
-Net.show()
-Net.learn( 'learn.txt' )
-print '==========IS LEARNED==========='
-Net.show()
 #'''
 
-print '==========DJANGO========='
+
 '''
-Net = NeuronNet( cntLayer = 3, nrsInLayer = [ 10, 5, 1 ], inputs = 1 )
+print '==========Тестируем сохранение в БД========='
+Net = NeuronNet( cntLayer = 4, nrsInLayer = [ 10, 10, 10, 10 ], inputs = 1 )
 print 'Net.calc( [90] ) = ' + str( Net.calc( [90] ) ) 
 print 'Net.calc( [30] ) = ' + str( Net.calc( [30] ) )
 print 'Net.calc( [0] ) = ' + str( Net.calc( [0] ) )
@@ -30,8 +27,9 @@ Net.saveDB()
 print 'Id in DB = ' + str( Net.getId() )
 #'''
 
-#'''
-Net = NeuronNet.loadDB( id = 5 )
+'''
+print '==========Тестируем загрузку из БД========='
+Net = NeuronNet.loadDB( id = 1 )
 print 'Net.calc( [90] ) = ' + str( Net.calc( [90] ) ) 
 print 'Net.calc( [30] ) = ' + str( Net.calc( [30] ) )
 print 'Net.calc( [0] ) = ' + str( Net.calc( [0] ) )
